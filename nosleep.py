b31d3f3c1da3fc3317b6870bd2782770ce3ad625
avtoritet <alex3ller@gmail.com>
2015-08-29 21:30:51 -0400

    Create nosleep.py

---
+++ nosleep.py
@@ -0,0 +1,17 @@
+# This short script will keep mouse-clicking every 60 sec
+# for n amount of minutes to not let computer fall asleep
+import win32api, win32con, time
+
+# Total time
+MINUTES = 60
+
+def click(x, y):
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+
+for i in range (0, MINUTES):
+    click(55, 0)
+    time.sleep(60)
+    click(85, 0)
+    time.sleep(60)
